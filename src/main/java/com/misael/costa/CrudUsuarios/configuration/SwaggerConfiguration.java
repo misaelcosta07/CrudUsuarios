54deb3207cdadd2e9aa9e5ad922ad012e0b4b9df
devMisa <misaelcosta069@gmail.com>
2025-12-18 17:45:51 -0300

    modifiquei o .properties, para sao subir os dados do meu DB, adicionei o exemple.properties

--- src/main/java/com/misael/costa/CrudUsuarios/configuration/SwaggerConfiguration.java
+++ src/main/java/com/misael/costa/CrudUsuarios/configuration/SwaggerConfiguration.java
@@ -1,10 +1,20 @@
 package com.misael.costa.CrudUsuarios.configuration;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SwaggerConfiguration {
+
+    @Bean
     public OpenAPI customApi(){
 
+        return new OpenAPI().info(new Info()
+                .title("Cadastros")
+                .version("1.0.0")
+                .description("Cadastro de usuarios.Apenas com nome e email, feito para ser basico e treino de API.")
+        );
     }
 }
--- src/main/java/com/misael/costa/CrudUsuarios/controller/UsersController.java
+++ src/main/java/com/misael/costa/CrudUsuarios/controller/UsersController.java
@@ -2,6 +2,8 @@ package com.misael.costa.CrudUsuarios.controller;
 
 import com.misael.costa.CrudUsuarios.entity.Users;
 import com.misael.costa.CrudUsuarios.service.UsersService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.AfterDomainEventPublication;
 import org.springframework.web.bind.annotation.*;
@@ -11,6 +13,7 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("user")
+@Tag(name = "cadastrados", description = "gerenciador de cadastro")
 public class UsersController {
 
     @Autowired
@@ -18,21 +21,25 @@ public class UsersController {
 
     // listar (get)
     @GetMapping("/list")
+    @Operation(summary = "Listar usuarios", description = "Lista todos os usuarios cadastrados, salvos no DB.")
     public List<Users> listUser(){
         return usersService.listUser();
     }
     // adicionar (post)
     @PostMapping("/create")
+    @Operation(summary = "Cradastrar usuarios", description = "Cria e cadastra o usuario no DB")
     public Users createUsers(@RequestBody Users user){
         return usersService.createUser(user);
     }
     // update - editar (put)
     @PutMapping("/{id}")
+    @Operation(summary = "Altera dados dos usuarios", description = "atualiza dados do usuario ja salvo no DB por ID.")
     public Users editarById(@PathVariable Long id, @RequestBody Users user) {
             return usersService.update(id, user);
     }
     // deletar (delete)
     @DeleteMapping("/{id}")
+    @Operation(summary = "Deleta usuarios", description = "deleta usuarios por ID criados no DB.")
     public void deleteById(@PathVariable Long id){
         usersService.deleteByID(id);
     }
